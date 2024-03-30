79f9a909177ded953873ff03da031f2d346a27a8
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2024-03-30 20:17:03 +0800

    Update MainActivity.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -326,16 +326,8 @@ class MainActivity : ThemedActivity(),
             R.id.nav_traffic -> displayFragment(WebviewFragment())
             R.id.nav_tools -> displayFragment(ToolsFragment())
             R.id.nav_logcat -> displayFragment(LogcatFragment())
-            R.id.nav_faq -> {
-                launchCustomTab("https://matsuridayo.github.io/")
-                return false
-            }
 
             R.id.nav_about -> displayFragment(AboutFragment())
-            R.id.nav_tuiguang -> {
-                launchCustomTab("https://matsuricom.pages.dev/")
-                return false
-            }
 
             else -> return false
         }
