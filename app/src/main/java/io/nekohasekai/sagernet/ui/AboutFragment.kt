8be3dec27bfc3bc201a525eb5326b86c2b60d1e8
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2024-05-12 14:16:56 +0800

    Update AboutFragment.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/AboutFragment.kt
@@ -93,16 +93,6 @@ class AboutFragment : ToolbarFragment(R.layout.layout_about) {
                         .subText(Libcore.versionBox())
                         .setOnClickAction { }
                         .build())
-                    .addItem(MaterialAboutActionItem.Builder()
-                        .icon(R.drawable.ic_baseline_card_giftcard_24)
-                        .text(R.string.donate)
-                        .subText(R.string.donate_info)
-                        .setOnClickAction {
-                            requireContext().launchCustomTab(
-                                "https://matsuridayo.github.io/index_docs/#donate"
-                            )
-                        }
-                        .build())
                     .apply {
                         PackageCache.awaitLoadSync()
                         for ((_, pkg) in PackageCache.installedPluginPackages) {
@@ -154,27 +144,7 @@ class AboutFragment : ToolbarFragment(R.layout.layout_about) {
                         }
                     }
                     .build())
-                .addCard(MaterialAboutCard.Builder()
-                    .outline(false)
-                    .title(R.string.project)
-                    .addItem(MaterialAboutActionItem.Builder()
-                        .icon(R.drawable.ic_baseline_sanitizer_24)
-                        .text(R.string.github)
-                        .setOnClickAction {
-                            requireContext().launchCustomTab(
-                                "https://github.com/MatsuriDayo/NekoBoxForAndroid"
 
-                            )
-                        }
-                        .build())
-                    .addItem(MaterialAboutActionItem.Builder()
-                        .icon(R.drawable.ic_qu_shadowsocks_foreground)
-                        .text(R.string.telegram)
-                        .setOnClickAction {
-                            requireContext().launchCustomTab(
-                                "https://t.me/MatsuriDayo"
-                            )
-                        }
                         .build())
                     .build())
                 .build()
