f76ce640541a0bf29a188de4628990b17828a3d0
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2024-09-16 16:09:13 +0800

    chore: update libcore and build process

--- buildScript/lib/core/get_source.sh
+++ buildScript/lib/core/get_source.sh
@@ -13,22 +13,8 @@ pushd ..
 ####
 
 if [ ! -d "sing-box" ]; then
-  git clone --no-checkout https://github.com/MatsuriDayo/sing-box.git
+  git clone https://github.com/Restia-Ashbell/sing-box
 fi
-pushd sing-box
-git checkout "$COMMIT_SING_BOX"
-
-popd
-
-####
-
-if [ ! -d "libneko" ]; then
-  git clone --no-checkout https://github.com/MatsuriDayo/libneko.git
-fi
-pushd libneko
-git checkout "$COMMIT_LIBNEKO"
-
-popd
 
 ####
 
