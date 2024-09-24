2797183b73e4859d942e0fb15378c6142ae85004
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2024-09-24 22:26:15 +0800

    chore: improve workflows and update Java JDK

--- libcore/build.sh
+++ libcore/build.sh
@@ -2,7 +2,8 @@
 
 source ../buildScript/init/env_ndk.sh
 
-gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -w -buildid=' -tags='with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech,with_shadowsocksr' . || exit 1
+VERSION=$(curl --silent "https://api.github.com/repos/sagernet/sing-box/releases" | jq -r 'sort_by(.published_at) | .[-1].tag_name')
+gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags="-X github.com/sagernet/sing-box/constant.Version=${VERSION} -s -w -buildid=" -tags="with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech,with_shadowsocksr" . || exit 1
 rm -r libcore-sources.jar
 
 proj=../app/libs
