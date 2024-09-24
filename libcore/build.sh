e4d746475acce9e5542a772fb36c4a99c140a751
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2024-09-24 22:26:15 +0800

    chore: improve workflows and update Java JDK

--- libcore/build.sh
+++ libcore/build.sh
@@ -2,7 +2,7 @@
 
 source ../buildScript/init/env_ndk.sh
 
-gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags='-s -w -buildid=' -tags='with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech,with_shadowsocksr' . || exit 1
+gomobile bind -v -androidapi 21 -trimpath -buildvcs=false -ldflags="-X github.com/sagernet/sing-box/constant.Version=$(git rev-parse --short HEAD) -s -w -buildid=" -tags="with_conntrack,with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api,with_ech,with_shadowsocksr" . || exit 1
 rm -r libcore-sources.jar
 
 proj=../app/libs
--- libcore/init.sh
+++ libcore/init.sh
@@ -2,16 +2,8 @@
 
 # Install gomobile
 if [ ! -f "$GOPATH/bin/gomobile" ]; then
-    git clone --depth 1 https://github.com/SagerNet/gomobile.git
-    pushd gomobile/cmd
-    pushd gomobile
-    go install -v
-    popd
-    pushd gobind
-    go install -v
-    popd
-    popd
-    rm -rf gomobile
+    go install -v github.com/sagernet/gomobile/cmd/gomobile
+    go install -v github.com/sagernet/gomobile/cmd/gobind
 fi
 
 gomobile init
