b0792336f2dd1b7e83ae03d1a7fa6975e2a95a9d
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2025-02-25 00:25:15 +0800

    chore: update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ apply(from = "../repositories.gradle.kts")
 
 dependencies {
     // Gradle Plugins
-    implementation("com.android.tools.build:gradle:8.7.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    implementation("com.android.tools.build:gradle:8.10.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
 }
