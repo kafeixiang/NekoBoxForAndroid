201481097f1b071b78e6d5d89cb433d8539349ea
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2025-03-03 16:34:57 +0800

    feat: adapt to ClashAPI Tracker

--- app/src/main/java/io/nekohasekai/sagernet/bg/proto/TrafficLooper.kt
+++ app/src/main/java/io/nekohasekai/sagernet/bg/proto/TrafficLooper.kt
@@ -128,7 +128,6 @@ class TrafficLooper
                 trafficUpdater = TrafficUpdater(
                     box = proxy.box, items = idMap.values.toList()
                 )
-                proxy.box.setV2rayStats(tags.joinToString("\n"))
             }
 
             trafficUpdater.updateAll()
