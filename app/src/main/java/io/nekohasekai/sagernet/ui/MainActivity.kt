85b86a53a35446de861967b4559b77afb2621634
kafeixiang <tingfengyuwo@gmail.com>
2025-06-12 18:49:00 +0800

    qmqg

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -325,10 +325,6 @@ class MainActivity : ThemedActivity(),
             R.id.nav_traffic -> displayFragment(WebviewFragment())
             R.id.nav_tools -> displayFragment(ToolsFragment())
             R.id.nav_logcat -> displayFragment(LogcatFragment())
-            R.id.nav_faq -> {
-                launchCustomTab("https://matsuridayo.github.io/")
-                return false
-            }
 
             R.id.nav_about -> displayFragment(AboutFragment())
 
